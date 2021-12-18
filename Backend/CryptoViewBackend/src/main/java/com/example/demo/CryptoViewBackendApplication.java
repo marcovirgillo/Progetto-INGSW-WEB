67400f2ed72bb63d205b565cb8f537763ada4308
simoven <simo10@live.it>
2021-12-18 19:30:16 +0100

    Project created

---
+++ Backend/CryptoViewBackend/src/main/java/com/example/demo/CryptoViewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoViewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoViewBackendApplication.class, args);
+	}
+
+}
