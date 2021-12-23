9cddb5bdfe2421301b3c301d85a57d8a9778ce62
pierobassa <pierobassa222@gmail.com>
2021-12-23 17:33:18 +0100

    sidebar fixed
    sidebar fixed

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
-import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
-
-import icon from '../images/logo-glow-rsz.png';
+import { PieChartOutlined, AppstoreOutlined, HomeOutlined, ReadOutlined, ProfileOutlined, MenuOutlined } from '@ant-design/icons';
+import icon from '../images/logo.png'; 
 
 const NavBar = () => {
     const [activeMenu, setActiveMenu] = useState(true);
@@ -33,7 +32,7 @@ const NavBar = () => {
         <div className="nav-container">
             <div className="logo-container">
                 <Avatar src={icon} size="large" />
-                <Typography.Title level={4} className="logo">
+                <Typography.Title level={2} className="logo">
                     <Link to="/">CryptoView</Link>
                 </Typography.Title>
                 <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}> {/* Clicking the button shows or hides the menu */}
@@ -41,23 +40,27 @@ const NavBar = () => {
                 </Button>
             </div>
             {activeMenu && ( //If activeMenu is true then I render the menu like this:
-                <Menu theme="dark">
-                <Menu.Item icon={<HomeOutlined />}>
-                    <Link to="/">Home</Link> 
-                </Menu.Item>
+                <Menu className='menu'>
+                        <Menu.Item style={{marginBottom:'15px'}} icon={<HomeOutlined className='menu-item'/>}>
+                            <Link  to="/">Home</Link> 
+                        </Menu.Item>
 
-                <Menu.Item icon={<FundOutlined />}>
-                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-                </Menu.Item>
+                        <Menu.Item style={{marginBottom:'15px'}} icon={<PieChartOutlined className='menu-item'/>}>
+                                <Link to="/portfolio">Portfolio</Link>
+                        </Menu.Item>
 
-                <Menu.Item icon={<MoneyCollectOutlined />}>
-                    <Link to="/exchanges">Exchanges</Link>
-                </Menu.Item>
-                
-                <Menu.Item icon={<BulbOutlined />}>
-                    <Link to="/news">News</Link>
-                </Menu.Item>
-            </Menu>
+                        <Menu.Item style={{marginBottom:'15px'}} icon={<AppstoreOutlined className='menu-item'/>}>
+                            <Link to="/dashboard">My Dashboard</Link>
+                        </Menu.Item>
+                        
+                        <Menu.Item style={{marginBottom:'15px'}} icon={<ReadOutlined className='menu-item'/>}>
+                            <Link to="/news">News</Link>    
+                        </Menu.Item>
+
+                        <Menu.Item style={{marginBottom:'15px'}} icon={<ProfileOutlined className='menu-item'/>}>
+                            <Link to="/exchanges">Exchanges</Link>    
+                        </Menu.Item>
+                </Menu> 
             )}
             
         </div>
