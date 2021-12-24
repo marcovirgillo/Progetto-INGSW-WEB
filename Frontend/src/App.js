137bd681460c9b6d2e0133a91d735289c4d57d1d
simoven <simo10@live.it>
2021-12-24 16:25:14 +0100

    Work on sidebar

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,6 @@
 import SideBar from './components/SideBar/SideBar';
 import './App.css';
+import './assets/main.css'
 import { BrowserRouter, Route } from 'react-router-dom';
 import AppRoutes from './components/AppRoutes';
 
--- Frontend/src/components/SideBar/SideBar.jsx
+++ Frontend/src/components/SideBar/SideBar.jsx
@@ -1,30 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { SidebarData } from './SidebarData';
 import { Link } from 'react-router-dom'
 import { Icon } from '@mui/material';
 
 import "./SideBar.css"
 
-const SideBarItem = (props) => {
-    const isActive = window.location.pathname == props.link;
-    return (
-        <ul key={props.Lkey} className="side-bar-item">
-            <li> <Icon sx={{marginRight: 1}}> {isActive ? props.blueLogo : props.logo} </Icon> </li>
-            <li> <Link to={props.link}>{props.text}</Link> </li>
-        </ul>
-    );
-} 
+const getClassnameForItem = (isActive) => {
+    let name = "side-bar-item ";
+    if(isActive)
+        name = name + "active";
+
+    return name;
+}
 
 export default function SideBar() {
+    const SideBarItem = (props) => {
+        const isActive = itemActive === props.text;
+    
+        return (
+            <ul className={getClassnameForItem(isActive)}>
+                <li> <Icon sx={{marginRight: 2}}> {isActive ? props.blueLogo : props.logo} </Icon> </li>
+                <li> <Link to={props.link} onClick={() => props.setItemActive(props.text)}>{props.text}</Link> </li>
+            </ul>
+        );
+    } 
+
+    const [itemActive, setItemActive] = useState(null);
+
     return(
         <div className="side-bar">
+            <div className="side-bar-logo">
+                <ul className="side-bar-logo-list">
+                    <img src={require("../../res/logos/CryptoViewLogo.png")}  alt="logo-cryptoview" height={80} width={80} /> 
+                    <div className="sidebar-logo-text"> <Link to="">CryptoView</Link> </div>
+                </ul>
+                
+            </div>
             <ul className="side-bar-list">
-                {SidebarData.map((item, val) => {
-                    console.log(item);
-                    console.log(val);
-
-                    return <SideBarItem Lkey={val} logo={item.icon} blueLogo={item.blueIcon} link={item.link} text={item.title} />
-                })}
+                {SidebarData.map((item, val) => (
+                     <SideBarItem key={val} logo={item.icon} blueLogo={item.blueIcon} link={item.link} text={item.title} setItemActive={setItemActive}/>
+                ))}
             </ul>
         </div>
     );
