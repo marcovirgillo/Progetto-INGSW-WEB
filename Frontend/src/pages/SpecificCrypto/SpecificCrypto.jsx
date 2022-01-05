c4512f25095f817a4f83b473d4f8405d4dcf568d
pierobassa <pierobassa222@gmail.com>
2022-01-05 22:35:07 +0100

    added icons and initial header for specific crypto
    added icons and initial header for specific crypto

--- Frontend/src/pages/SpecificCrypto/SpecificCrypto.jsx
+++ Frontend/src/pages/SpecificCrypto/SpecificCrypto.jsx
@@ -1,141 +1,137 @@
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router';
-import CryptoChart from '../../components/Chart/CryptoChart';
 import './SpecificCrypto.css';
 import "./../../App.css";
+import { Grid    } from '@mui/material'
+import ChartSection from './ChartSection'
 
-let crypto_prices = [];
-let market_caps = [];
-let volumes = [];
+import { info } from './TestData.js';
 
 const SpecificCrypto = () => {
-    const [chartType, setChartType] = useState("price");
-    const [chartInterval, setChartInterval] = useState("1");
 
-    //per avere l'id della crypto da visualizzare
-    const location = useLocation()
-    const cryptoID = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
-
-    const [chartData, setChartData] = useState([{name: 'Price', data: []}]);
-    const [chartDatetime, setChartDatetime] = useState([]);
-
-    const fetcher = () => {
-        fetch(`https://api.coingecko.com/api/v3/coins/${cryptoID}/market_chart?vs_currency=usd&days=${chartInterval}`)
-        .then((res) => res.json())
-        .then((result) => processData(result));
-    }
-
-    useEffect(() => {
-        fetcher();
-    }, []);
-
-    //se cambia l'intervallo di tempo, fetcho i dati nuovi
-    useEffect(() =>{
-        fetcher();
-    }, [chartInterval]);
-
-    //ogni volta che cambia il tipo di chart (price, cap, volume), imposto i dati nuovi che erano stati già fetchati
-    useEffect(checkChartType, [chartType]);
-
-    function checkChartType() {
-        if(chartType === "price") {
-            setChartData([{
-                name: 'Price',
-                data: crypto_prices
-            }]);
-        }
-
-        if(chartType === "market_cap") {
-            setChartData([{
-                name: 'Market Cap',
-                data: market_caps
-            }]);
-        }
-
-        if(chartType === "volume") {
-            setChartData([{
-                name: 'Volume 24h',
-                data: volumes
-            }]);
-        }
-    }
-
-    function processData(res) {
-        crypto_prices = [];
-        market_caps = [];
-        volumes = [];
-        let prices = res["prices"];
-        let caps = res["market_caps"]
-        let vols = res["total_volumes"]
-        let times = [];
-
-        prices.forEach((item) => {
-            if(item[1]>1)
-                crypto_prices.push(item[1].toFixed(2));
-            else
-                crypto_prices.push(item[1].toFixed(6));
-
-            times.push(item[0]);
-        })
-        
-
-        caps.forEach((item) => {
-            market_caps.push(item[1].toFixed(2));
-        })
-
-        vols.forEach((item) => {
-            volumes.push(item[1].toFixed(2));
-        })
-
-        setChartDatetime(times);
-        //dopo il fetch, controllo che tipo di chart era attivo
-        checkChartType();
-    }
-
-    function isBtnActive(name) {
-        return (name === chartType ? 'btn-active' : '');
-    }
-
-    function isBtnChartActive(name) {
-        return (name === chartInterval ? 'btn-active' : '');
-    }
-
-    const ChartButtons = (props) => {
-        return (
-            <ul className={"btn-container chart-btn-controller " + props.className}>
-                <p className={isBtnChartActive("1")} onClick={() => setChartInterval("1")}>24h</p>
-                <p className={isBtnChartActive("7")} onClick={() => setChartInterval("7")}>7D</p>
-                <p className={isBtnChartActive("30")} onClick={() => setChartInterval("30")}>30D</p>
-                <p className={isBtnChartActive("90")} onClick={() => setChartInterval("90")}>90D</p>
-                <p className={isBtnChartActive("365")} onClick={() => setChartInterval("365")}>1Y</p>
-                <p className={isBtnChartActive("max")} onClick={() => setChartInterval("max")}>ALL</p>
-            </ul>
-        )
-    }
+    console.log("RENDER")
 
     return (
         <div className="specific-crypto">
             <div className="paper-grey">
-                <div className="chart-div">
-                    <ul className="btn-containers-list">
-                        <ul className="btn-container">
-                            <p className={isBtnActive("price")} onClick={() => setChartType("price")}>Price</p>
-                            <p className={isBtnActive("market_cap")} onClick={() => setChartType("market_cap")}>Market cap</p>
-                            <p className={isBtnActive("volume")} onClick={() => setChartType("volume")}>Volume</p>
-                        </ul>
-                        <div className="h-spacer" />
-                        <ChartButtons className="chart-btns-desktop"/>
-                    </ul>
-                    <CryptoChart 
-                        className="chart"
-                        width="100%" 
-                        heigth="100%"
-                        data={chartData} 
-                        timestamps={chartDatetime} 
-                        color="#32C0FF"
-                    />  
-                    <ChartButtons className="chart-btns-mobile"/>
-                </div>
+                <div style={{paddingTop:'20px'}}/>
+                <Grid container columnSpacing={{xl:5, lg:5, md:2, sm:1, xs:2}} columns={{xl:20, lg:28, md:28, sm:7, xs:7}}> 
+                    <Grid item xl={1} lg={1} md={1} sm={1} xs={1} /> 
+                    <Grid className="item" item xl={6} lg={12} md={12} sm={5} xs={5}> 
+                        <div className="container-header-left">
+                            <ul>
+                                <ul className="container-title">
+                                    <img src={info.image.small} />
+                                    <div className='crypto-title'>{info.name}</div>
+                                    <p className='grey-container' style={{marginLeft:'10px'}}>{info.symbol.toUpperCase()}</p>
+                                    <p className="p-crypto" style={{marginLeft:'15px'}}>Rank #{info.market_cap_rank}</p>
+                                </ul>
+                            </ul>
+                            <ul>
+                                <ul className="container-title" style={{marginTop:'25px'}}>
+                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                                        <img src={require("../../res/logos/link.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                                        <div className="list-title-crypto" style={{marginBottom:'2px'}}>{info.links.homepage[0]}</div>
+                                    </p>
+                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                                        <img src={require("../../res/logos/explore.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                                        <div className="list-title-crypto" style={{marginBottom:'2px'}}>Explorers</div>
+                                    </p>
+                                </ul>
+                            </ul>
+                            <ul>
+                                <ul className="container-title" style={{marginTop:'10px'}}>
+                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                                        <img src={require("../../res/logos/github.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                                        <div className="list-title-crypto" style={{marginBottom:'2px'}}>GitHub</div>
+                                    </p>
+                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                                        <img src={require("../../res/logos/chat.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                                        <div className="list-title-crypto" style={{marginBottom:'2px'}}>Chat</div>
+                                    </p>
+                                </ul>
+                            </ul>
+                        </div>
+                    </Grid>
+                    <Grid item className="xs-spacer-crypto" sx={{display:'none'}} sm={1} xs={1} /> {/* Spaziatura per schermi più piccoli */}
+                    <Grid item className="xs-spacer-crypto" sx={{display:'none'}} sm={1} xs={1} />
+                    <Grid className="item" item xl={6} lg={12} md={12} sm={5} xs={5}> 
+                    <div className="container-header-left">
+                            <ul>
+                                <ul className="container-title">
+                                    <img src={info.image.small} />
+                                    <div className='crypto-title'>{info.name}</div>
+                                    <p className='grey-container' style={{marginLeft:'10px'}}>{info.symbol.toUpperCase()}</p>
+                                    <p className="p-crypto" style={{marginLeft:'15px'}}>Rank #{info.market_cap_rank}</p>
+                                </ul>
+                            </ul>
+                            <ul>
+                                <ul className="container-title" style={{marginTop:'25px'}}>
+                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                                        <img src={require("../../res/logos/link.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                                        <div className="list-title-crypto" style={{marginBottom:'2px'}}>{info.links.homepage[0]}</div>
+                                    </p>
+                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                                        <img src={require("../../res/logos/explore.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                                        <div className="list-title-crypto" style={{marginBottom:'2px'}}>Explorers</div>
+                                    </p>
+                                </ul>
+                            </ul>
+                            <ul>
+                                <ul className="container-title" style={{marginTop:'10px'}}>
+                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                                        <img src={require("../../res/logos/github.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                                        <div className="list-title-crypto" style={{marginBottom:'2px'}}>GitHub</div>
+                                    </p>
+                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                                        <img src={require("../../res/logos/chat.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                                        <div className="list-title-crypto" style={{marginBottom:'2px'}}>Chat</div>
+                                    </p>
+                                </ul>
+                            </ul>
+                        </div>
+                    </Grid>
+                    <Grid item className="xs-spacer-crypto" sx={{display:'none'}} sm={1} xs={1} />
+                    <Grid item className="xs-spacer-crypto" sx={{display:'none'}} sm={1} xs={1} /> {/* md={8}  TO CENTER BOTTOM ONE*/}
+                    <Grid className="item" item xl={6} lg={12} md={12} sm={5} xs={5}> 
+                    <div className="container-header-left">
+                            <ul>
+                                <ul className="container-title">
+                                    <img src={info.image.small} />
+                                    <div className='crypto-title'>{info.name}</div>
+                                    <p className='grey-container' style={{marginLeft:'10px'}}>{info.symbol.toUpperCase()}</p>
+                                    <p className="p-crypto" style={{marginLeft:'15px'}}>Rank #{info.market_cap_rank}</p>
+                                </ul>
+                            </ul>
+                            <ul>
+                                <ul className="container-title" style={{marginTop:'25px'}}>
+                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                                        <img src={require("../../res/logos/link.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                                        <div className="list-title-crypto" style={{marginBottom:'2px'}}>{info.links.homepage[0]}</div>
+                                    </p>
+                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                                        <img src={require("../../res/logos/explore.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                                        <div className="list-title-crypto" style={{marginBottom:'2px'}}>Explorers</div>
+                                    </p>
+                                </ul>
+                            </ul>
+                            <ul>
+                                <ul className="container-title" style={{marginTop:'10px'}}>
+                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                                        <img src={require("../../res/logos/github.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                                        <div className="list-title-crypto" style={{marginBottom:'2px'}}>GitHub</div>
+                                    </p>
+                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                                        <img src={require("../../res/logos/chat.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                                        <div className="list-title-crypto" style={{marginBottom:'2px'}}>Chat</div>
+                                    </p>
+                                </ul>
+                            </ul>
+                        </div>
+                    </Grid>
+                    <Grid item xl={1} lg={1} md={1} sm={1} xs={1} />
+                </Grid>
+                <div style={{paddingTop:'20px'}}/>
+                <ChartSection />
             </div>
             
         </div>
---
+++ Frontend/src/pages/SpecificCrypto/TestData.js
@@ -0,0 +1,129 @@
+export const info = {
+    "id": "polkadot",
+    "symbol": "dot",
+    "name": "Polkadot",
+    "links": {
+        "homepage": [
+          "https://polkadot.network/",
+          "",
+          ""
+        ],
+        "blockchain_site": [
+          "http://polkascan.io/",
+          "https://polkadot-cc1.subscan.io/",
+          "https://hubble.figment.io/polkadot/chains/polkadot",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          ""
+        ],
+        "official_forum_url": [
+          "",
+          "",
+          ""
+        ],
+        "chat_url": [
+          "https://discord.com/invite/wGUDt2p",
+          "",
+          ""
+        ],
+        "announcement_url": [
+          "",
+          ""
+        ],
+        "twitter_screen_name": "Polkadot",
+        "facebook_username": "",
+        "bitcointalk_thread_identifier": null,
+        "telegram_channel_identifier": "PolkadotOfficial",
+        "subreddit_url": "https://www.reddit.com",
+        "repos_url": {
+          "github": [
+            "https://github.com/paritytech/polkadot"
+          ],
+          "bitbucket": []
+        }
+      },
+      "image": {
+        "thumb": "https://assets.coingecko.com/coins/images/12171/thumb/polkadot.png?1639712644",
+        "small": "https://assets.coingecko.com/coins/images/12171/small/polkadot.png?1639712644",
+        "large": "https://assets.coingecko.com/coins/images/12171/large/polkadot.png?1639712644"
+      },
+      "current_price": 29.74,
+      "market_cap": 31858473950,
+      "market_cap_rank": 9,
+      "total_volume": 1112749687,
+      "price_change_percentage_24h": -0.98659,
+      "price_change_24h_in_currency": {
+        "btc": -3.47355e-7,
+        "eth": 0.00003908
+      },
+      "tickers": [
+        {
+          "base": "DOT",
+          "target": "USDT",
+          "market": {
+            "name": "WOO Network",
+            "identifier": "wootrade",
+            "has_trading_incentive": false
+          },
+          "last": 29.81,
+          "volume": 335910.86,
+          "converted_last": {
+            "btc": 0.00063982,
+            "eth": 0.0078089,
+            "usd": 29.77
+          },
+          "converted_volume": {
+            "btc": 214.922,
+            "eth": 2623,
+            "usd": 10000195
+          },
+          "trust_score": "green",
+          "bid_ask_spread_percentage": 0.03019,
+          "timestamp": "2022-01-05T16:41:05+00:00",
+          "last_traded_at": "2022-01-05T16:41:05+00:00",
+          "last_fetch_at": "2022-01-05T16:41:05+00:00",
+          "is_anomaly": false,
+          "is_stale": false,
+          "trade_url": "https://x.woo.network/spot",
+          "token_info_url": null,
+          "coin_id": "polkadot",
+          "target_coin_id": "tether"
+        },
+        {
+          "base": "DOT",
+          "target": "USDT",
+          "market": {
+            "name": "Binance",
+            "identifier": "binance",
+            "has_trading_incentive": false
+          },
+          "last": 29.77,
+          "volume": 9133510.73951965,
+          "converted_last": {
+            "btc": 0.00063896,
+            "eth": 0.00780334,
+            "usd": 29.71
+          },
+          "converted_volume": {
+            "btc": 5836,
+            "eth": 71272,
+            "usd": 271331238
+          },
+          "trust_score": "green",
+          "bid_ask_spread_percentage": 0.03358,
+          "timestamp": "2022-01-05T16:47:03+00:00",
+          "last_traded_at": "2022-01-05T16:47:03+00:00",
+          "last_fetch_at": "2022-01-05T16:47:03+00:00",
+          "is_anomaly": false,
+          "is_stale": false,
+          "trade_url": "https://www.binance.com/en/trade/DOT_USDT?ref=37754157",
+          "token_info_url": null,
+          "coin_id": "polkadot",
+          "target_coin_id": "tether"
+        },
+      ]
+}
