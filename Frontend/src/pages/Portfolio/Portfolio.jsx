b47335ddf3d7fa7e2ad8e26e0e9cdeb5d23b24b2
simoven <simo10@live.it>
2022-01-05 11:55:41 +0100

    portfolio done

--- Frontend/src/pages/Portfolio/Portfolio.jsx
+++ Frontend/src/pages/Portfolio/Portfolio.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import CryptoChart from '../../components/Chart/CryptoChart';
 import { PortfolioData } from './Data.js';
+import PortfolioTable from './PortfolioTable';
 import "./../../App.css";
 import "./Portfolio.css";
+import plus_icon from "./../../res/logos/plus.png";
 
 const greenColor = "#46C95B";
 const redColor = "#E05757";
@@ -36,19 +38,18 @@ const Portfolio = () => {
     }
 
     function processData(res) {
-        let prices = res ["prices"];
-        let values = new Array();
-        let times = new Array();
-        prices.map((item) => {
-            values.push(item [1].toFixed(2));
-            times.push(item [0]);
+        let prices = res["prices"];
+        let values = [];
+        let times = [];
+        prices.forEach((item) => {
+            values.push(item[1].toFixed(2));
+            times.push(item[0]);
         })
 
         setCryptoPrices([{
             name: "Price",
             data: values
         }])
-
        
         setCryptoDatetime(times);
     }
@@ -73,41 +74,61 @@ const Portfolio = () => {
         )
     }
 
+    const AddNewButton = () => {
+        return (
+            <div className="add-new-btn">
+                <ul className="inline-list">
+                    <img src={plus_icon} width={24} height={24} alt="icon"/>
+                    <p className="white-label btn-label">Add New</p>
+                </ul>
+            </div>
+        );
+    }
+
 
     return (
         <div className="portfolio">
             <div className="paper-grey">
-            <h4 className="name-label">{PortfolioData.portfolio_name}</h4>
-            <ul className="portfolio-list">
-                <h3 className="current-balance-label">Current Balance</h3>
-                <ul className="inline-list">
-                    <p className="current-balance">{PortfolioData.balance}</p>
-                    <IndicatorRectangle price_change={PortfolioData.balance_change_24h_percentage}/>
-                </ul>
-                <ul className="inline-list">
-                    <p className={getClassNameChange(PortfolioData.balance_change_24h)}>{PortfolioData.balance_change_24h}</p>
-                    <p className="label-24h">24h</p>
-                </ul>
-            </ul>
-            <div className="chart-div" style={{marginTop: '20px'}}>
-                <ul className="btn-containers-list">
-                    <ul className="btn-container">
-                        <p className={isBtnActive("chart")} onClick={() => setChartType("chart")}>Chart</p>
-                        <p className={isBtnActive("allocation")} onClick={() => setChartType("allocation")}>Allocation</p>
-                        <p className={isBtnActive("statistics")} onClick={() => setChartType("statistics")}>Statistics</p>
+                <h4 className="name-label">{PortfolioData.portfolio_name}</h4>
+                <ul className="portfolio-list">
+                    <h3 className="current-balance-label">Current Balance</h3>
+                    <ul className="inline-list">
+                        <p className="current-balance">{PortfolioData.balance}</p>
+                        <IndicatorRectangle price_change={PortfolioData.balance_change_24h_percentage}/>
                     </ul>
-                    <div className="h-spacer" />
-                    { chartType === 'chart' && (<ChartButtons className="chart-btns-desktop"/>) }
+                    <ul className="inline-list">
+                        <p className={getClassNameChange(PortfolioData.balance_change_24h)}>{PortfolioData.balance_change_24h}</p>
+                        <p className="label-24h">24h</p>
+                    </ul>
                 </ul>
-                <CryptoChart className="chart"
-                    color={greenColor} 
-                    width="100%" 
-                    height="120%"
-                    data={cryptoPrices} 
-                    timestamps={cryptoDatetime} 
-                />
-                { chartType === 'chart' && (<ChartButtons className="chart-btns-mobile"/>) }
-            </div>
+                <div className="chart-div" style={{marginTop: '20px'}}>
+                    <ul className="btn-containers-list">
+                        <ul className="btn-container">
+                            <p className={isBtnActive("chart")} onClick={() => setChartType("chart")}>Chart</p>
+                            <p className={isBtnActive("allocation")} onClick={() => setChartType("allocation")}>Allocation</p>
+                            <p className={isBtnActive("statistics")} onClick={() => setChartType("statistics")}>Statistics</p>
+                        </ul>
+                        <div className="h-spacer" />
+                        { chartType === 'chart' && (<ChartButtons className="chart-btns-desktop"/>) }
+                    </ul>
+                    <CryptoChart className="chart"
+                        color={greenColor} 
+                        width="100%" 
+                        height="120%"
+                        data={cryptoPrices} 
+                        timestamps={cryptoDatetime} 
+                    />
+                    { chartType === 'chart' && (<ChartButtons className="chart-btns-mobile"/>) }
+                </div>
+                <ul className="assets-list">
+                    <p className="white-label assets-label">My Assets</p>
+                    <div className="h-spacer" />
+                    <AddNewButton />
+                </ul>
+                <ul style={{display: 'flex', flexDirection: 'columns', padding: 0, margin: 0,
+                            justifyContent: 'center', alignItems: 'flex-start'}}>
+                    <PortfolioTable />
+                </ul>
             </div>
         </div>
     )
