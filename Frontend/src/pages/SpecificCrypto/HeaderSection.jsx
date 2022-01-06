b56a6e528a897e86c2dee6b4af9eeae7737b6c77
pierobassa <pierobassa222@gmail.com>
2022-01-06 18:13:02 +0100

    initial responsive for the main crypto data section
    initial responsive for the main crypto data section

--- Frontend/src/pages/SpecificCrypto/HeaderSection.jsx
+++ Frontend/src/pages/SpecificCrypto/HeaderSection.jsx
@@ -64,6 +64,101 @@ const HeaderSection = () => {
         return cryptoData.length !== 0;
     }
 
+    const CommunitySection = () => {
+        return (
+            <div className='community-info-container'>
+                 <ul>
+                     <p className="p-crypto" style={{color:'#ABABAB'}}>Community</p>
+                     <ul className="container-title" style={{marginTop:'20px'}}>
+                         <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                             <img src={require("../../res/logos/twitter.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                             <a href={`https://twitter.com/${cryptoData.links.twitter_screen_name}`}> <span className="list-title-crypto" style={{marginBottom:'2px'}}>Twitter</span> </a>
+                         </p>
+                     </ul>
+                 </ul>
+                 <ul>
+                     <ul className="container-title" style={{marginTop:'15px'}}>
+                         <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                             <img src={require("../../res/logos/telegram.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                             <a href={`https://t.me/${cryptoData.links.telegram_channel_identifier}`}> <span className="list-title-crypto" style={{marginBottom:'2px'}}>Telegram</span> </a>
+                         </p>
+                     </ul>
+                 </ul>
+                 <ul>
+                     <ul className="container-title" style={{marginTop:'15px'}}>
+                         <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                             <img src={require("../../res/logos/reddit.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                             <a href={cryptoData.links.subreddit_url}> <span className="list-title-crypto" style={{marginBottom:'2px'}}>Reddit</span> </a>
+                         </p>
+                     </ul>
+                 </ul>
+            </div>
+        )
+    }
+
+    const PriceDataSection = () => {
+        return (
+            <div className='prices-info-container'>
+                <ul>
+                    <p className="p-crypto" style={{color:'#ABABAB'}}>{cryptoData.name} {cryptoData.symbol.toUpperCase()} Price</p>
+                    <ul className="container-title">
+                        <div className='crypto-title'>{getFormattedPrice(cryptoData.market_data.current_price.usd)}</div>
+                        <p className={getChangeClass(cryptoData.market_data.price_change_percentage_24h)} style={{marginLeft:'10px'}}>{change(cryptoData.market_data.price_change_percentage_24h)}%</p>
+                        <p className="p-crypto" style={{marginLeft:'15px'}}>24h</p>
+                    </ul>
+                </ul>
+                <ul>
+                    <ul className="container-title">
+                        <p className="p-crypto" style={{color:'#ABABAB', marginLeft:'18px', marginTop:'0px'}}>{cryptoData.market_data.current_price.btc} BTC</p>
+                        <p className={getChangeClassWithoutContainer(cryptoData.market_data.price_change_percentage_24h_in_currency.btc)} style={{marginLeft:'18px', marginTop:'0px'}}>{change(cryptoData.market_data.price_change_percentage_24h_in_currency.btc)}%</p>
+                    </ul>
+                </ul>
+                <ul>
+                    <ul className="container-title" style={{marginTop:'-20px'}}>
+                        <p className="p-crypto" style={{color:'#ABABAB', marginLeft:'18px', marginTop:'0px'}}>{cryptoData.market_data.current_price.eth} ETH</p>
+                        <p className={getChangeClassWithoutContainer(cryptoData.market_data.price_change_percentage_24h_in_currency.eth)} style={{marginLeft:'18px', marginTop:'0px'}}>{change(cryptoData.market_data.price_change_percentage_24h_in_currency.eth)}%</p>
+                    </ul>
+                </ul>
+            </div>
+        )
+    }
+
+    const MainDetailsSection = () => {
+        return (
+            <div className="container-header">
+                <ul>
+                    <ul className="container-title">
+                        <img src={cryptoData.image.small} />
+                        <div className='crypto-title'>{cryptoData.name}</div>
+                        <p className='grey-container' style={{marginLeft:'10px'}}>{cryptoData.symbol.toUpperCase()}</p>
+                        <p className="p-crypto" style={{marginLeft:'15px'}}>{screenSize>600 ? 'Rank' : ''} #{cryptoData.market_cap_rank}</p>
+                    </ul>
+                </ul>
+                <ul>
+                    <ul className="container-title" style={{marginTop:'25px'}}>
+                        <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                            <img src={require("../../res/logos/link.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                            <a href={cryptoData.links.homepage[0]}> <span className="list-title-crypto" style={{marginBottom:'2px'}}>{cryptoData.links.homepage[0]}</span> </a>
+                        </p>
+                    
+                    </ul>
+                </ul>
+                <ul>
+                    <ul className="container-title" style={{marginTop:'10px'}}>
+                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                            <img src={require("../../res/logos/explore.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                            <a href={cryptoData.links.blockchain_site[0]}> <span className="list-title-crypto" style={{marginBottom:'2px'}}>Explorer</span> </a>
+                        </p>
+                        <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
+                            <img src={require("../../res/logos/github.png")} width={26} height={26} style={{marginRight:'10px'}}/>
+                            <a href={cryptoData.links.repos_url.github[0]}> <span className="list-title-crypto" style={{marginBottom:'2px'}}>GitHub</span> </a>
+                        </p>
+                    </ul>
+                </ul>
+            </div>
+        )
+    }
+
     return (
         <React.Fragment>
         {
@@ -71,95 +166,25 @@ const HeaderSection = () => {
                 <Grid container columnSpacing={{xl:5, lg:5, md:2, sm:1, xs:2}} columns={{xl:20, lg:28, md:28, sm:28, xs:7}} > 
                     <Grid item xl={1} lg={1} md={1} sm={1} xs={1} /> 
                     <Grid className="item" item xl={6} lg={12} md={12} sm={12} xs={5}> 
-                        <div className="container-header">
-                            <ul>
-                                <ul className="container-title">
-                                    <img src={cryptoData.image.small} />
-                                    <div className='crypto-title'>{cryptoData.name}</div>
-                                    <p className='grey-container' style={{marginLeft:'10px'}}>{cryptoData.symbol.toUpperCase()}</p>
-                                    <p className="p-crypto" style={{marginLeft:'15px'}}>{screenSize>600 ? 'Rank' : ''} #{cryptoData.market_cap_rank}</p>
-                                </ul>
-                            </ul>
-                            <ul>
-                                <ul className="container-title" style={{marginTop:'25px'}}>
-                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
-                                        <img src={require("../../res/logos/link.png")} width={26} height={26} style={{marginRight:'10px'}}/>
-                                        <a href={cryptoData.links.homepage[0]}> <span className="list-title-crypto" style={{marginBottom:'2px'}}>{cryptoData.links.homepage[0]}</span> </a>
-                                    </p>
-                                
-                                </ul>
-                            </ul>
-                            <ul>
-                                <ul className="container-title" style={{marginTop:'10px'}}>
-                                <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
-                                        <img src={require("../../res/logos/explore.png")} width={26} height={26} style={{marginRight:'10px'}}/>
-                                        <a href={cryptoData.links.blockchain_site[0]}> <span className="list-title-crypto" style={{marginBottom:'2px'}}>Explorer</span> </a>
-                                    </p>
-                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
-                                        <img src={require("../../res/logos/github.png")} width={26} height={26} style={{marginRight:'10px'}}/>
-                                        <a href={cryptoData.links.repos_url.github[0]}> <span className="list-title-crypto" style={{marginBottom:'2px'}}>GitHub</span> </a>
-                                    </p>
-                                </ul>
-                            </ul>
-                        </div>
+                        <MainDetailsSection />
                     </Grid>
                     <Grid item className="xs-spacer-crypto" sx={{display:'none'}} sm={1}  xs={1}/> {/* Spaziatura per schermi più piccoli */}
                     <Grid item className="xs-spacer-crypto" sx={{display:'none'}} sm={1}  />
                     <Grid className="item" item xl={6} lg={12} md={12} sm={12} xs={5}> 
-                        <div className='prices-info-container'>
-                            <ul>
-                                <p className="p-crypto" style={{color:'#ABABAB'}}>{cryptoData.name} {cryptoData.symbol.toUpperCase()} Price</p>
-                                <ul className="container-title">
-                                    <div className='crypto-title'>{getFormattedPrice(cryptoData.market_data.current_price.usd)}</div>
-                                    <p className={getChangeClass(cryptoData.market_data.price_change_percentage_24h)} style={{marginLeft:'10px'}}>{change(cryptoData.market_data.price_change_percentage_24h)}%</p>
-                                    <p className="p-crypto" style={{marginLeft:'15px'}}>24h</p>
-                                </ul>
-                            </ul>
-                            <ul>
-                                <ul className="container-title">
-                                    <p className="p-crypto" style={{color:'#ABABAB', marginLeft:'18px', marginTop:'0px'}}>{cryptoData.market_data.current_price.btc} BTC</p>
-                                    <p className={getChangeClassWithoutContainer(cryptoData.market_data.price_change_percentage_24h_in_currency.btc)} style={{marginLeft:'18px', marginTop:'0px'}}>{change(cryptoData.market_data.price_change_percentage_24h_in_currency.btc)}%</p>
-                                </ul>
-                            </ul>
-                            <ul>
-                                <ul className="container-title" style={{marginTop:'-20px'}}>
-                                    <p className="p-crypto" style={{color:'#ABABAB', marginLeft:'18px', marginTop:'0px'}}>{cryptoData.market_data.current_price.eth} ETH</p>
-                                    <p className={getChangeClassWithoutContainer(cryptoData.market_data.price_change_percentage_24h_in_currency.eth)} style={{marginLeft:'18px', marginTop:'0px'}}>{change(cryptoData.market_data.price_change_percentage_24h_in_currency.eth)}%</p>
-                                </ul>
-                            </ul>
-                        </div>
+                    {
+                        screenSize<600 ? <CommunitySection /> : <PriceDataSection />
+                    }
                     </Grid>
                     <Grid item className="xs-spacer-crypto" sx={{display:'none'}} sm={1} xs={1} />
-                    <Grid item className="xs-spacer-crypto" sx={{display:'none'}} sm={1} xs={1} /> {/* md={8}  TO CENTER BOTTOM ONE*/}
+                    {
+                        screenSize >= 600 && (
+                            <Grid item className="xs-spacer-crypto" sx={{display:'none'}} sm={1} xs={1} />
+                        )
+                    }
                     <Grid className="item" item xl={6} lg={12} md={12} sm={12} xs={5}> 
-                    {screenSize>900 &&
-                    <div className='community-info-container'>
-                            <ul>
-                                <p className="p-crypto" style={{color:'#ABABAB'}}>Community</p>
-                                <ul className="container-title" style={{marginTop:'20px'}}>
-                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
-                                        <img src={require("../../res/logos/twitter.png")} width={26} height={26} style={{marginRight:'10px'}}/>
-                                        <a href={`https://twitter.com/${cryptoData.links.twitter_screen_name}`}> <span className="list-title-crypto" style={{marginBottom:'2px'}}>Twitter</span> </a>
-                                    </p>
-                                </ul>
-                            </ul>
-                            <ul>
-                                <ul className="container-title" style={{marginTop:'15px'}}>
-                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
-                                        <img src={require("../../res/logos/telegram.png")} width={26} height={26} style={{marginRight:'10px'}}/>
-                                        <a href={`https://t.me/${cryptoData.links.telegram_channel_identifier}`}> <span className="list-title-crypto" style={{marginBottom:'2px'}}>Telegram</span> </a>
-                                    </p>
-                                </ul>
-                            </ul>
-                            <ul>
-                                <ul className="container-title" style={{marginTop:'15px'}}>
-                                    <p className='specific-crypto-primary-btn container-title' style={{marginLeft:'10px', cursor:'pointer'}}>
-                                        <img src={require("../../res/logos/reddit.png")} width={26} height={26} style={{marginRight:'10px'}}/>
-                                        <a href={cryptoData.links.subreddit_url}> <span className="list-title-crypto" style={{marginBottom:'2px'}}>Reddit</span> </a>
-                                    </p>
-                                </ul>
-                            </ul>
-                        </div>
+                    
+                    {
+                        screenSize<600 ? <PriceDataSection /> : <CommunitySection />
                     }
                     </Grid>
                     <Grid item xl={1} lg={1} md={1} sm={1} xs={1} />
--- Frontend/src/pages/SpecificCrypto/SpecificCrypto.jsx
+++ Frontend/src/pages/SpecificCrypto/SpecificCrypto.jsx
@@ -3,12 +3,30 @@ import './SpecificCrypto.css';
 import "./../../App.css";
 import ChartSection from './ChartSection'
 import HeaderSection from './HeaderSection'
+import { Grid } from '@mui/material'
 
 const SpecificCrypto = () => {
+    const [screenSize, setScreenSize] = useState(null);
 
     useEffect(() => {
         window.scrollTo(0, 0)
     }, [])
+
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        
+        return () => window.removeEventListener('resize', handleResize);
+    });
+
+    function twoByTwoGrid() {
+        return screenSize > 600 && screenSize <= 1536;
+    }
+
+    function mobileGrid() {
+        return screenSize <= 600;
+    }
 
     return (
         <div className="specific-crypto">
@@ -16,6 +34,49 @@ const SpecificCrypto = () => {
                 <div style={{paddingTop:'20px'}}/>
                 <HeaderSection />
                 <div style={{paddingTop:'20px'}}/>
+                <Grid container columnSpacing={{xl:5, lg:5, md:2, sm:1, xs:2}} columns={{xl:28, lg:28, md:28, sm:28, xs:7}} > 
+                    <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
+                    <Grid className="item" item xl={6} lg={12} md={12} sm={12} xs={5}> 
+                        
+                    </Grid>
+                    {mobileGrid() && (
+                            <>
+                                <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
+                                <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
+                            </>
+                        )
+                    }   
+                    <Grid className="item" item xl={6} lg={12} md={12} sm={12} xs={5}> 
+                    
+                    </Grid>
+                    {twoByTwoGrid() && (
+                            <>
+                                <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
+                                <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
+                            </>
+                        )
+                    }
+                    {mobileGrid() && (
+                            <>
+                                <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
+                                <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
+                            </>
+                        )
+                    }   
+                    <Grid className="item" item xl={6} lg={12} md={12} sm={12} xs={5}> 
+                        
+                    </Grid>
+                    {mobileGrid() && (
+                            <>
+                                <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
+                                <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
+                            </>
+                        )
+                    }   
+                    <Grid className="item" item xl={6} lg={12} md={12} sm={12} xs={5}> 
+                        
+                    </Grid>
+                </Grid>
                 <ChartSection />
             </div>
             
