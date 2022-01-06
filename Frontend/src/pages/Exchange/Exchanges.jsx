5c8313e1545364ca287aaa49084cd44830649665
simoven <simo10@live.it>
2022-01-06 15:10:42 +0100

    fixed piero push

--- Frontend/src/pages/Exchange/Exchanges.jsx
+++ Frontend/src/pages/Exchange/Exchanges.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ExchangesTable from './ExchangesTable'
-import NewsSection from '../Home/NewsSection'
+import NewsExchangesSection from './NewsExchangeSection'
 
 const Exchanges = () => {
     return (
         <div className="exchanges-page">
             <div className="paper-grey">
                 <h4 className="news-day-label">Exchanges news of the day</h4>
-                <NewsSection />
+                <NewsExchangesSection />
                 <p className="exchanges-title">Top Cryptocurrency Exchanges</p>
                 <ul style={{padding: 0, margin: 0, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                    <ExchangesTable /> {/* Tabella all criptos */}
+                    <ExchangesTable /> 
                 </ul>
             </div>
         </div>
