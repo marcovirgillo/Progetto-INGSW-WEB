915b4d7c80d6ccd826cc95f233c29d83f389571f
Marco Virgillo <marcovirgillo10x@gmail.com>
2022-01-06 18:43:22 +0100

    News responsivvvv

--- Frontend/src/pages/News/News.jsx
+++ Frontend/src/pages/News/News.jsx
@@ -5,11 +5,12 @@ import { NewsData } from '../Home/TestData'
 
 const BigNewsBox = (props) => {
     return (
-        <div className='big-news-box'>
-            <ul className='list-big-news'>
+        <div className='big-news-container'>
+            <ul className='big-news-list'>
                 <img src={props.imagePath} className='big-news-image'/>
                 <p className='big-news-title'> {props.title} </p>
                 <p className='big-news-description'> {props.content} </p>
+                <p className='big-news-date'> {props.date} </p>
             </ul>
         </div>
     );
@@ -17,12 +18,41 @@ const BigNewsBox = (props) => {
 
 const SideNewsBox = (props) => {
     return (
-        <ul className='side-news-container' >
-            <ul className='side-news-list'>
-                <p className='side-news-title'> {props.title} </p>
-                <img src={props.imagePath} className='side-news-image'/>
-            </ul>
-        </ul>
+        <div className='three-side-news-container'>
+
+            <div className='single-side-news-container'>
+                <ul className='single-side-news-list'>
+                    <div className='side-news-list-title-date'>
+                        <p className='side-news-title'> {props.title} </p>
+                        <p className='side-news-date'> {props.date} </p>
+                    </div>
+                    <img src={props.imagePath} className='side-news-image'/>
+
+                </ul>
+            </div>
+            
+            <div className='single-side-news-container'>
+                <ul className='single-side-news-list'>
+                    <div className='side-news-list-title-date'>
+                        <p className='side-news-title'> {props.title} </p>
+                        <p className='side-news-date'> {props.date} </p>
+                    </div>
+                    <img src={props.imagePath} className='side-news-image'/>
+
+                </ul>
+            </div>
+
+            <div className='single-side-news-container'>
+                <ul className='single-side-news-list'>
+                    <div className='side-news-list-title-date'>
+                        <p className='side-news-title'> {props.title} </p>
+                        <p className='side-news-date'> {props.date} </p>
+                    </div>
+                    <img src={props.imagePath} className='side-news-image'/>
+
+                </ul>
+            </div>
+        </div>
     );
 }
 
@@ -36,16 +66,17 @@ export default function News() {
                <BigNewsBox
                     imagePath = {NewsData[0].image}
                     title = {NewsData[0].title}
-                    content = {NewsData[0].content}
+                    content = {"Dirigente di Coinbase: presto arriveranno miglioramenti importanti sulla scalabilità di Ethereum Dirigente di Coinbase: presto arriveranno miglioramenti importanti sulla scalabilità di Ethereum Dirigente di Coinbase: presto arriveranno miglioramenti importanti sulla scalabilità di Ethereum Dirigente di Coinbase: presto arriveranno miglioramenti importanti sulla scalabilità di Ethereum Dirigente di Coinbase: presto arriveranno miglioramenti importanti sulla scalabilità di Ethereum Dirigente di Coinbase: presto arriveranno miglioramenti importanti sulla scalabilità di Ethereum "}
+                    date = {"06/01/2022"}
                />
-               
-               <SideNewsBox 
-                    title = {NewsData[1].title}
+              
+                <SideNewsBox 
+                    title = {"Dirigente di Coinbase: presto arriveranno miglioramenti importanti sulla scalabilità di Ethereum"}
                     imagePath = {NewsData[1].image}
-               />
+                    date = {"06/01/2022"}
+                />
 
             </div>
-
         </div>
     );
 }
