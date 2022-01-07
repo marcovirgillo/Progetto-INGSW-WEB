98c4bb0e26d54085bf486a862d48660e69386077
pierobassa <pierobassa222@gmail.com>
2022-01-07 16:02:39 +0100

    Further improvements specific crypto and temporary fix for news API

--- Backend/src/main/java/com/cryptoview/model/api/API.java
+++ Backend/src/main/java/com/cryptoview/model/api/API.java
@@ -3,16 +3,21 @@ package com.cryptoview.model.api;
 public class API {
 
 	public static API instance = null;
-	private final String newsAPIKey = "5b515b57ab5945328c3e6b2a0456aded";
+	private final String newsAPIKey[] = {"5b515b57ab5945328c3e6b2a0456aded", "5d711f7e4dc7434bb70e91331a269d34"};
 	private final String newsAPIPart1 = "https://newsapi.org/v2/everything?domains=cointelegraph.com&pageSize=";
 	private final String newsAPIPart2 = "&apiKey=";
-	private final String newsExchangesAPI = "https://newsapi.org/v2/everything?domains=cointelegraph.com&pageSize=8&q=exchange&apiKey=5b515b57ab5945328c3e6b2a0456aded";
+	private final String newsExchangesAPI = "https://newsapi.org/v2/everything?domains=cointelegraph.com&pageSize=8&q=exchange&apiKey=";
+	
 	private final String topAPIPart1 = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=";
 	private final String topAPIPart2 = "&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d";
+	
 	private final String marketStatsAPI = "https://api.coingecko.com/api/v3/global";
+	
 	private final String exchangesAPI = "https://api.coingecko.com/api/v3/exchanges";
+	
 	private final String criptoDataAPIPart1 = "https://api.coingecko.com/api/v3/coins/";
 	private final String criptoDatatAPIpart2 = "?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false";
+	
 	private final String criptoHistoryAPIPart1 = "https://api.coingecko.com/api/v3/coins/";
 	private final String cryptoHistoryAPIPart2 = "/market_chart?vs_currency=usd&days=";
 			
@@ -25,7 +30,7 @@ public class API {
 	}
 	
 	public String getNewsAPI(int number) {
-		return newsAPIPart1 + number + newsAPIPart2 + newsAPIKey;
+		return newsAPIPart1 + number + newsAPIPart2 + newsAPIKey[1];
 	}
 	
 	public String getTopAPI(int number) {
@@ -49,6 +54,6 @@ public class API {
 	}
 	
 	public String getNewsExchangesAPI() {
-		return newsExchangesAPI;
+		return newsExchangesAPI + newsAPIKey[1];
 	}
 }
--- Frontend/src/pages/SpecificCrypto/SpecificCrypto.jsx
+++ Frontend/src/pages/SpecificCrypto/SpecificCrypto.jsx
@@ -3,13 +3,13 @@ import './SpecificCrypto.css';
 import "./../../App.css";
 import ChartSection from './ChartSection'
 import HeaderSection from './HeaderSection'
+import StatisticsSection from './StatisticsSection'
 import { Grid } from '@mui/material'
 import { useLocation } from 'react-router';
 
 const SpecificCrypto = () => {
     const [screenSize, setScreenSize] = useState(null);
     const [cryptoData, setCryptoData] = useState([]);
-    const [volume24hData, setVolume24hData] = useState([]);
 
     const location = useLocation()
     const cryptoID = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
@@ -21,16 +21,8 @@ const SpecificCrypto = () => {
         (error) => console.log("Error fetching crypto"));
     }
 
-    const volumeFetcher = () => {
-        fetch(`https://api.coingecko.com/api/v3/coins/${cryptoID}/market_chart?vs_currency=usd&days=1`)
-        .then((res) => res.json())
-        .then((result) => setVolume24hData(result.total_volumes),
-        (error) => console.log("Error fetching 24h volume"));
-    }
-
     useEffect(() => {
         fetcher();
-        volumeFetcher();
     }, []);
 
     useEffect(() => {
@@ -45,142 +37,13 @@ const SpecificCrypto = () => {
         return () => window.removeEventListener('resize', handleResize);
     });
 
-    const formatter = new Intl.NumberFormat('en-US', {style: 'currency', currency:'USD'});
-
-    function twoByTwoGrid() {
-        return screenSize > 600 && screenSize <= 1536;
-    }
-
-    function mobileGrid() {
-        return screenSize <= 600;
-    }
-
-    function setStyleContainer(position){
-        if(screenSize > 600 && screenSize <= 1536){
-            if(position==="left" || position==="first-left")
-                return {borderTopLeftRadius:'15px', borderBottomLeftRadius: '15px', borderRight:'2px solid #333333'};
-            else if(position==="right" || position==="last-right")
-                return {borderTopRightRadius:'15px', borderBottomRightRadius: '15px'};
-        }
-        if(screenSize <= 600)
-            return {borderRadius:'15px'};
-        if(position==="first-left")
-            return {borderTopLeftRadius:'15px', borderBottomLeftRadius: '15px', borderRight:'2px solid #333333'};
-        if(position==="last-right")
-            return {borderTopRightRadius:'15px', borderBottomRightRadius: '15px'};
-        if(position==="right")
-            return {borderRight:'2px solid #333333'};
-        if(position==="left")
-            return {borderRight:'2px solid #333333'};
-    }
-
-    function getFormattedPrice(price) {
-        if(price > 1)
-            return formatter.format(price);
-        else 
-            return "$" + price;
-    }
-
-    function change(change) {
-        if(change > 0){
-            return "+" + change.toFixed(2);
-        }
-        
-        return change.toFixed(2);
-    }
-
-    const cryptoDataSize = () => {
-        return cryptoData.length !== 0 && volume24hData.length !==0;
-    }
-
-    function getChangeClass(change){
-        if(change>=0)
-            return "green-container";
-        return "red-container";
-    }
-    
-    function getVolumeChange(){
-        let yesterdayVol = volume24hData[0][1];
-        let dim = volume24hData.length -1;
-        let todayVol = volume24hData[dim][1];
-        
-        let change = 100 - ((yesterdayVol * 100) / todayVol);
-
-        return change;
-    }
-
     return (
         <div className="specific-crypto">
             <div className="paper-grey">
                 <div style={{paddingTop:'20px'}}/>
                 <HeaderSection data={cryptoData}/>
                 <div style={{paddingTop:'20px'}}/>
-                <React.Fragment>
-                {
-                cryptoDataSize() && (
-                    <Grid container columnSpacing={{xl:5, lg:5, md:2, sm:1, xs:2}} columns={{xl:28, lg:28, md:28, sm:28, xs:7}} > 
-                        <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
-                        <Grid className="item" item xl={6} lg={12} md={12} sm={12} xs={5} style={{paddingLeft:'0px', paddingRight:'0px'}}> 
-                            <div className="crypto-main-stats-4x" style={setStyleContainer("first-left")}>
-                                <div className="crypto-main-container-title">Market cap</div>
-                                <div className='crypto-main-container-value' style={{marginTop:'10px'}}>{getFormattedPrice(cryptoData.market_data.market_cap.usd)}</div>
-                                <div className='crypto-main-container-change' style={{marginTop:'15px'}}>
-                                    <span className={getChangeClass(cryptoData.market_data.market_cap_change_percentage_24h)} style={{marginLeft:'10px'}}>{change(cryptoData.market_data.market_cap_change_percentage_24h)}%</span>
-                                </div>
-                            </div>
-                        </Grid>
-                        {mobileGrid() && (
-                                <>
-                                    <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
-                                    <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
-                                </>
-                            )
-                        }   
-                        <Grid className="item" item xl={6} lg={12} md={12} sm={12} xs={5} style={{paddingLeft:'0px', paddingRight:'0px'}}> 
-                            <div className="crypto-main-stats-4x" style={setStyleContainer("right")}>
-                                <div className="crypto-main-container-title">All Time High & Difference</div>
-                                <div className='crypto-main-container-value' style={{marginTop:'10px'}}>{getFormattedPrice(cryptoData.market_data.ath.usd)}</div>
-                                <div className='crypto-main-container-change' style={{marginTop:'15px'}}>
-                                    <span className={getChangeClass(cryptoData.market_data.ath_change_percentage.usd)} style={{marginLeft:'10px'}}>{change(cryptoData.market_data.ath_change_percentage.usd)}%</span>
-                                </div>
-                            </div>
-                        </Grid>
-                        {twoByTwoGrid() && (
-                                <>
-                                    <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
-                                    <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
-                                </>
-                            )
-                        }
-                        {mobileGrid() && (
-                                <>
-                                    <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
-                                    <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
-                                </>
-                            )
-                        }   
-                        <Grid className="item" item xl={6} lg={12} md={12} sm={12} xs={5} style={{paddingLeft:'0px', paddingRight:'0px'}}> 
-                            <div className="crypto-main-stats-4x" style={setStyleContainer("left")}>
-                                <div className="crypto-main-container-title">Volume 24h</div>
-                                <div>{getVolumeChange()}</div>
-                            </div>
-                        </Grid>
-                        {mobileGrid() && (
-                                <>
-                                    <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
-                                    <Grid item xl={2} lg={2} md={2} sm={2} xs={1} /> 
-                                </>
-                            )
-                        }   
-                        <Grid className="item" item xl={6} lg={12} md={12} sm={12} xs={5} style={{paddingLeft:'0px', paddingRight:'0px'}}> 
-                            <div className="crypto-main-stats-4x"  style={setStyleContainer("last-right")}>
-                                <div className="crypto-main-container-title">Circulating supply</div>
-                            </div>
-                        </Grid>
-                    </Grid>
-                    )
-                }
-                </React.Fragment>
+                <StatisticsSection data={cryptoData} />
                 <ChartSection />
             </div>
             
