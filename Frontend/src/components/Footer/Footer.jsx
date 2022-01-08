92d8757674203937fb1fe8881fea9f5a85287ade
pierobassa <pierobassa222@gmail.com>
2022-01-08 17:39:03 +0100

    Finished footer, links needed. Added about us page with routing

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Footer.css'
 import { Link } from 'react-router-dom'
 
-const Footer = (props) => {
+const Footer = () => {
 
     return (
         <div className='footer'>
@@ -15,9 +15,14 @@ const Footer = (props) => {
                 </div>
             </div>
             <div className="footer-pages">
-                <span>Home</span>
-                <span>My Dashboard</span>
-                <span>Portfolio</span>
+                <div className="footer-list-pages">
+                    <Link to=""><span className="footer-pages-text" style={{cursor:'pointer'}}>Home</span></Link>
+                    <Link to="/portfolio"><span className="footer-pages-text" style={{cursor:'pointer'}}>Portfolio</span></Link>
+                    <Link to="/dashboard"><span className="footer-pages-text" style={{cursor:'pointer'}}>My Dashboard</span></Link>
+                    <Link to="/news"><span className="footer-pages-text" style={{cursor:'pointer'}}>News</span></Link>
+                    <Link to="/exchanges"><span className="footer-pages-text" style={{cursor:'pointer'}}>Exchanges</span></Link>
+                    <Link to="/aboutus"><span className="footer-pages-text" style={{cursor:'pointer'}}>About us</span></Link>
+                </div>
             </div>
             <div style={{paddingTop:'20px'}} />
             <div className="footer-socials">
@@ -27,9 +32,14 @@ const Footer = (props) => {
                     <img src={require("../../res/logos/paypal.png")}  alt="paypal-cryptoview" height={32} width={32} style={{marginLeft:'10px'}}/> 
                 </div>
             </div>
-            <div className="footer-rights">
-                
+            <div style={{paddingTop:'20px'}} />
+            <div className="footer-rights">    
+                <div className="footer-list-rights">
+                    <span className="footer-rights-text" style={{color:'white'}}>© All rights reserved.</span>
+                    <span className="footer-rights-text">Cryptoview 2022</span>
+                </div>
             </div>
+            <div style={{paddingTop:'20px'}} />
         </div>
     )
 }
