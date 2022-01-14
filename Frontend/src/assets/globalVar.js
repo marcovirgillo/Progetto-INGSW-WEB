5106b4a8372d18130dd87ef611bbba7601770862
simoven <simo10@live.it>
2022-01-14 18:03:13 +0100

    merge

--- Frontend/src/assets/globalVar.js
+++ Frontend/src/assets/globalVar.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-export const address = "localhost";
-=======
-export const address = "192.168.1.148";
->>>>>>> c83fb1a18fbde775dfeb6adf7f37e46a5a891234
+export const address = "192.168.43.232";
 
