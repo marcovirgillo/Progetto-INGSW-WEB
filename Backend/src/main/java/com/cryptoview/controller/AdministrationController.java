8d041b1b455cb787a58e9a8e2074faad23ad924c
simoven <simo10@live.it>
2022-02-05 12:34:05 +0100

    merge

--- Backend/src/main/java/com/cryptoview/controller/AdministrationController.java
+++ Backend/src/main/java/com/cryptoview/controller/AdministrationController.java
@@ -10,12 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cryptoview.persistence.dao.CryptoDaoJDBC;
